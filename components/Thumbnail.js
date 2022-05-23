b5561251754ecbe8c11b44c030cf00cd2f8b260a
soukaynaBen <49325238+soukaynaBen@users.noreply.github.com>
2022-05-23 16:31:16 +0200

    adjust title

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -28,7 +28,7 @@ function Thumbnail({result}) {
             />
             <div className='p-2'>
                 <p className='truncate max-w-md'>{result.overview}</p>
-                <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
+                <h2 className="mt-1 text-xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
                 <p className=" flex items-center opacity-0 group-hover:opacity-100">
                 {result.media_type && `${result.media_type} *`}{" "}
                 {result.release_date || result.first_air_date }* {" "} <ThumbUpIcon  className="h-5 mx-2"/> {result.vote_count}
